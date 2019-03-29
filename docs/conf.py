01346597ead713a60ae9b6504edca158947bf818
Jared <flamableconcrete@gmail.com>
2019-03-29 14:10:41 +0000

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,8 @@ project = 'thumbtack-client'
 copyright = '2019, The MITRE Corporation'
 author = 'The MITRE Corporation'
 
-version = '0.1.0'
-release = '0.1.0'
+version = '0.1.1'
+release = '0.1.1'
 
 extensions = [
     'sphinx.ext.autodoc',
