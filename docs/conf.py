3b460d34c25c159f1d8f5a4be597b11f301cef34
Jared Ondricek <jondricek@mitre.org>
2021-08-15 23:04:57 -0500

    Black Autoformatter

--- docs/conf.py
+++ docs/conf.py
@@ -1,64 +1,74 @@
 # -*- coding: utf-8 -*-
 
-project = 'thumbtack-client'
-copyright = '2019, The MITRE Corporation'
-author = 'The MITRE Corporation'
+project = "thumbtack-client"
+copyright = "2019, The MITRE Corporation"
+author = "The MITRE Corporation"
 
-version = '0.3.0'
-release = '0.3.0'
+version = "0.3.0"
+release = "0.3.0"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-master_doc = 'index'
+master_doc = "index"
 
 language = None
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     # 'canonical_url': '',
     # 'analytics_id': '',
     # 'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
     # 'style_external_links': False,
     # 'vcs_pageview_mode': '',
     # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 2,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 2,
     # 'includehidden': True,
     # 'titles_only': False
 }
 
-htmlhelp_basename = 'thumbtack-clientdoc'
+htmlhelp_basename = "thumbtack-clientdoc"
 
 latex_elements = {}
 
 latex_documents = [
-    (master_doc, 'thumbtack-client.tex', 'thumbtack-client Documentation',
-     'The MITRE Corporation', 'manual'),
+    (
+        master_doc,
+        "thumbtack-client.tex",
+        "thumbtack-client Documentation",
+        "The MITRE Corporation",
+        "manual",
+    ),
 ]
 
 man_pages = [
-    (master_doc, 'thumbtack-client', 'thumbtack-client Documentation',
-     [author], 1)
+    (master_doc, "thumbtack-client", "thumbtack-client Documentation", [author], 1)
 ]
 
 texinfo_documents = [
-    (master_doc, 'thumbtack-client', 'thumbtack-client Documentation',
-     author, 'thumbtack-client', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "thumbtack-client",
+        "thumbtack-client Documentation",
+        author,
+        "thumbtack-client",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
--- setup.py
+++ setup.py
@@ -4,50 +4,52 @@ import os
 from setuptools import setup, find_packages
 
 # Package meta-data.
-NAME = 'thumbtack_client'
-DESCRIPTION = 'Connect to a Thumbtack Server.'
-URL = 'https://github.com/mitre/thumbtack-client'
-EMAIL = 'thumbtack@mitre.org'
-AUTHOR = 'The MITRE Corporation'
-LICENSE = 'Apache 2.0'
-REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '0.3.0'
+NAME = "thumbtack_client"
+DESCRIPTION = "Connect to a Thumbtack Server."
+URL = "https://github.com/mitre/thumbtack-client"
+EMAIL = "thumbtack@mitre.org"
+AUTHOR = "The MITRE Corporation"
+LICENSE = "Apache 2.0"
+REQUIRES_PYTHON = ">=2.7.0"
+VERSION = "0.3.0"
 
-REQUIRED = [
-    'requests'
-]
+REQUIRED = ["requests"]
 
 doc_requires = [
-    'sphinx',
+    "sphinx",
 ]
 
 test_requires = [
-    'coverage',
-    'pytest',
-    'pytest-cov',
-    'responses',
+    "coverage",
+    "pytest",
+    "pytest-cov",
+    "responses",
 ]
 
-dev_requires = doc_requires + test_requires + [
-    'bumpversion',
-    'check-manifest',
-    # test_requires are installed into every tox environment, so we don't
-    # want to include tox there.
-    'tox',
-]
+dev_requires = (
+    doc_requires
+    + test_requires
+    + [
+        "bumpversion",
+        "check-manifest",
+        # test_requires are installed into every tox environment, so we don't
+        # want to include tox there.
+        "tox",
+    ]
+)
 
 EXTRAS = {
-    'dev': dev_requires,
-    'docs': doc_requires,
-    'test': test_requires,
+    "dev": dev_requires,
+    "docs": doc_requires,
+    "test": test_requires,
 }
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 try:
-    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -57,26 +59,26 @@ setup(
     description=DESCRIPTION,
     license=LICENSE,
     long_description=long_description,
-    long_description_content_type='text/x-rst',
+    long_description_content_type="text/x-rst",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Security',
-        'Topic :: System :: Filesystems',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
+        "Topic :: System :: Filesystems",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='imagemounter api thumbtack',
+    keywords="imagemounter api thumbtack",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=REQUIRED,
